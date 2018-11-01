bec0ce8d5af957b8b5047a854a693439c18ebe33
FurySwordXD <sainathganesh1@gmail.com>
2018-11-01 20:35:32 +0530

    meh

--- Server.py
+++ Server.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template, send_from_directory, request
 from pathlib import Path
 import os, json
 import re
+import tkinter as tk
+from tkinter import filedialog
+from flask_cors import CORS
 
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askdirectory()
+print(file_path)
 app = Flask(__name__)
-
+CORS(app)
 def get_all_files(directory, file_types=[]):
     data = []
     reg_ex = "\.("
@@ -18,18 +26,21 @@ def get_all_files(directory, file_types=[]):
         
     for x in os.walk(directory):
         if len(x[2]) > 0:
-            folder = x[0]
+            root = x[0].replace("\\","/")
+            folders = []
+            for i in x[1]:
+                folders.append(i.replace("\\","/"))
             files = []
             for i in x[2]:
                 if rx.search(i):
                     files.append(i)
             if len(files) > 0:
-                data.append({"directory": folder, "files": files})
+                data.append({"directory": root, "folders": folders, "files": files})
     return data
 
 @app.route("/")
 def index():
-    data = get_all_files("D:\Videos\Initial D",["mp4"])
+    data = get_all_files(file_path)
         
     with open("data.json", "w") as file:
         json.dump(data, file)
@@ -42,4 +53,4 @@ def video():
     return send_from_directory(directory, video_file)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
